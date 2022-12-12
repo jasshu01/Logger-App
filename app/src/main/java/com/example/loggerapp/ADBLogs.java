be5fe06d7b57947382041fc988d23de5279f921d
jasshugarg <jashugarg266@gmail.com>
2022-12-12 09:23:43 +0530

    adb logs in separate activity

---
+++ app/src/main/java/com/example/loggerapp/ADBLogs.java
@@ -0,0 +1,130 @@
+package com.example.loggerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ADBLogs extends AppCompatActivity {
+
+    Button start, stop;
+    TextView tv;
+    int i = 0;
+    boolean flag = true;
+
+    @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.startCapturing);
+        stop = findViewById(R.id.stopCapturing);
+        tv = (TextView) findViewById(R.id.textView);
+
+        i = 0;
+        flag = true;
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flag = true;
+                Toast.makeText(ADBLogs.this, "Starting", Toast.LENGTH_SHORT).show();
+                capture();
+
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(ADBLogs.this, "Stopping", Toast.LENGTH_SHORT).show();
+                flag = false;
+            }
+        });
+
+    }
+
+
+    public void capture() {
+
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                while (flag) {
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            try {
+
+
+                                Runtime.getRuntime().exec("logcat - c");
+
+//                Log.e("tag", "hey , i am an error "+ i);
+                                Log.e("tag", "hey , i am an error " + i++);
+//            Process process = Runtime.getRuntime().exec("logcat");
+
+
+                                String[] command = new String[]{"logcat", "-d", "radio", "threadtime"};
+//                                String[] command = new String[]{"logcat", "-d", "-v", "threadtime"};
+//                                String[] command = new String[]{"logcat", "-b", "all", "threadtime"};
+                                Process process = Runtime.getRuntime().exec(command);
+
+
+                                BufferedReader bufferedReader = new BufferedReader(
+                                        new InputStreamReader(process.getInputStream()));
+
+                                StringBuilder log = new StringBuilder();
+                                String str = "";
+                                String line = "";
+                                while ((line = bufferedReader.readLine()) != null) {
+
+                                    String[] myline = line.split(" ");
+
+                                    str = line + "\n\n\t" + str;
+                                    log.append(line + " " + i);
+                                    log.append("\n\n\t");
+
+                                }
+
+                                tv.setText(str);
+
+                            } catch (IOException e) {
+
+                                tv.setText(e.toString());
+                                Log.e("tag", e.toString() + " " + e.getMessage() + " " + e.getLocalizedMessage());
+                                e.printStackTrace();
+                            }
+                        }
+
+                    });
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+            }
+        }.start();
+
+
+    }
+}
--- app/src/main/java/com/example/loggerapp/MainActivity.java
+++ app/src/main/java/com/example/loggerapp/MainActivity.java
@@ -3,24 +3,25 @@ package com.example.loggerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    Button start, stop;
-    TextView tv;
-    int i = 0;
-    boolean flag = true;
+    Button adblogs;
+
 
     @SuppressLint({"MissingInflatedId", "LocalSuppress"})
     @Override
@@ -28,98 +29,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start = findViewById(R.id.startCapturing);
-        stop = findViewById(R.id.stopCapturing);
-        tv = (TextView) findViewById(R.id.textView);
+        adblogs = findViewById(R.id.adblogs);
 
-        i = 0;
-        flag = true;
-
-        start.setOnClickListener(new View.OnClickListener() {
+        adblogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                flag=true;
-                capture();
-
+                startActivity(new Intent(MainActivity.this, ADBLogs.class));
             }
         });
 
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                flag = false;
-            }
-        });
-
-    }
-
-
-    public void capture() {
-
-
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                while (flag) {
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            try {
-
-
-                                Runtime.getRuntime().exec("logcat - c");
-
-//                Log.e("tag", "hey , i am an error "+ i);
-                                Log.e("tag", "hey , i am an error " + i++);
-//            Process process = Runtime.getRuntime().exec("logcat");
-
-
-                                String[] command = new String[]{"logcat", "-d", "-v", "threadtime"};
-                                Process process = Runtime.getRuntime().exec(command);
-
-
-                                BufferedReader bufferedReader = new BufferedReader(
-                                        new InputStreamReader(process.getInputStream()));
-
-                                StringBuilder log = new StringBuilder();
-                                String str = "";
-                                String line = "";
-                                while ((line = bufferedReader.readLine()) != null) {
-
-                                    str = line + "\n\n\t" + str;
-                                    log.append(line + " " + i);
-                                    log.append("\n\n\t");
-
-                                }
-
-                                tv.setText(str);
-
-                            } catch (IOException e) {
-
-                                tv.setText(e.toString());
-                                Log.e("tag", e.toString() + " " + e.getMessage() + " " + e.getLocalizedMessage());
-                                e.printStackTrace();
-                            }
-                        }
-
-                    });
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-
-                }
-
-            }
-        }.start();
 
 
     }
+
 }
 
